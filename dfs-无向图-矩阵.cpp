c76971f173790d626aeb5f4dd6e87af3c1214cab
VoidK2 <1399477504@qq.com>
2018-03-02 13:58:45 +0800

    Update dfs-无向图-矩阵.cpp

--- dfs-无向图-矩阵.cpp
+++ dfs-无向图-矩阵.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************************
-¡¾DFS¡¿
+【DFS】
 Author:tmw
 date:2018-2-19
 ********************************************************************************************/
@@ -9,49 +9,49 @@ date:2018-2-19
 
 
 #define MAX_VERTEX 100
-#define inf 65535  //±íÊ¾Á½µãÖ®¼äÃ»ÓÐ±ßÏàÁ¬
+#define inf 65535  //表示两点之间没有边相连
 
-int visit[MAX_VERTEX];   //±ê¼Ç¶¥µãÊÇ·ñ±»·ÃÎÊ
+int visit[MAX_VERTEX];   //标记顶点是否被访问
 
-/**Í¼µÄÁÚ½Ó¾ØÕóµÄ½¨Á¢**/
-//ÁÚ½Ó¾ØÕóÊý¾Ý½á¹¹¶¨Òå
+/**图的邻接矩阵的建立**/
+//邻接矩阵数据结构定义
 typedef struct Martrix_Graph
 {
-    char vertex[MAX_VERTEX]; //´æ´¢¶¥µãÐÅÏ¢
-    int edge[MAX_VERTEX][MAX_VERTEX]; //´æ´¢±ßÐÅÏ¢
-    int vertex_number,edge_number;//´æ´¢¶¥µãÊýºÍ±ßÊý
+    char vertex[MAX_VERTEX]; //存储顶点信息
+    int edge[MAX_VERTEX][MAX_VERTEX]; //存储边信息
+    int vertex_number,edge_number;//存储顶点数和边数
 }Martrix_Graph;
 
 void Create_non_direction_martrix_Graph( Martrix_Graph *G )
 {
     int i,j,k,m;
-    printf("ÇëÊäÈë¹¹ÔìµÄÎÞÏòÍ¼µÄ¶¥µãÊýºÍ±ßÊý£º\n");
+    printf("请输入构造的无向图的顶点数和边数：\n");
     scanf("%d %d",&G->vertex_number,&G->edge_number);
 
-    printf("ÇëÊäÈëÎÞÏòÍ¼¶¥µãÐÅÏ¢£¨ÈçABCDEF....£©£º\n");
+    printf("请输入无向图顶点信息（如ABCDEF....）：\n");
     char ch;
-    while( ( ch = getchar() != '\n' ) );  //¹ýÂËµôÇ°ÃæµÄ\n£¬·ÀÖ¹\n±»scanf½øÈ¥
+    while( ( ch = getchar() != '\n' ) );  //过滤掉前面的\n，防止\n被scanf进去
     for(i=0;i<G->vertex_number;i++)
         scanf("%c",&G->vertex[i]);
 
-    //²»ÏàÁ¬µÄ¶¥µãÖ®¼äµÄÈ¨ÖµÉèÎªinf£¬°üÀ¨¶¥µã×ÔÉí
-    //³õÊ¼»¯ÁÚ½Ó¾ØÕó
+    //不相连的顶点之间的权值设为inf，包括顶点自身
+    //初始化邻接矩阵
     for(i=0;i<G->vertex_number;i++)
         for(j=0;j<G->vertex_number;j++)
             G->edge[i][j] = inf;
 
-    //¸üÐÂÎÞÏòÍ¼±ßÐÅÏ¢
-    printf("ÇëÊäÈëÎÞÏòÍ¼ÁÚ½Ó¾ØÕóÏàÁ¬µÄ±ßÐÅÏ¢£¬ÏàÁ¬±ê¼ÇÎª1\n");
+    //更新无向图边信息
+    printf("请输入无向图邻接矩阵相连的边信息，相连标记为1\n");
     for(k=0;k<G->edge_number;k++)
     {
         scanf("%d %d %d",&i,&j,&m);
         G->edge[i][j] = m;
-        G->edge[j][i] = G->edge[i][j];//ÎÞÏòÍ¼ÊÇ¶Ô³ÆÕó
+        G->edge[j][i] = G->edge[i][j];//无向图是对称阵
     }
 
 
-    //´òÓ¡ÁÚ½Ó¾ØÕó´æ´¢ÐÅÏ¢£¬¼ì²éÕýÈ·ÐÔ
-    printf("---------------------¹¹Ôì³öÀ´µÄÎÞÏòÍ¼ÁÚ½Ó¾ØÕóÈçÏÂ---------------------\n");
+    //打印邻接矩阵存储信息，检查正确性
+    printf("---------------------构造出来的无向图邻接矩阵如下---------------------\n");
     for(i=0;i<G->vertex_number;i++)
     {
         for(j=0;j<G->vertex_number;j++)
@@ -60,31 +60,31 @@ void Create_non_direction_martrix_Graph( Martrix_Graph *G )
     }
 }
 
-//´Óµ±Ç°µÚi¸ö¶¥µã³ö·¢£¬DFS±éÀú
+//从当前第i个顶点出发，DFS遍历
 void DFS(Martrix_Graph G, int i)
 {
     int j;
-    //±ê¼Çµ±Ç°¶¥µãÎªÒÑ·ÃÎÊ×´Ì¬
+    //标记当前顶点为已访问状态
     visit[i] = true;
     printf("%c ",G.vertex[i]);
 
-    //¶ÔÓë¸Ã¶¥µãÏàÁ¬µÄÆäËûÎ´·ÃÎÊ¶¥µã½øÐÐDFS
+    //对与该顶点相连的其他未访问顶点进行DFS
     for(j=0;j<G.vertex_number;j++)
     {
         if( G.edge[i][j]==1 && !visit[j] )
             DFS(G,j);
     }
 }
-//¶ÔÕû¸öÍ¼½øÐÐDFS
+//对整个图进行DFS
 void DFS_Travel(Martrix_Graph G)
 {
-    //³õÊ¼»¯visitÊý×é
+    //初始化visit数组
     int i;
     for(i=0;i<G.vertex_number;i++)
         visit[i] = false;
 
-    //ÕûÕÅÍ¼µÄDFS
-    printf("¶Ô¸ÃÎÞÏòÍ¼½øÐÐDFSµÄ½á¹ûÈçÏÂ£º\n");
+    //整张图的DFS
+    printf("对该无向图进行DFS的结果如下：\n");
     for(i=0;i<G.vertex_number;i++)
     {
         if( !visit[i] )
@@ -93,7 +93,7 @@ void DFS_Travel(Martrix_Graph G)
 }
 int main()
 {
-    printf("²âÊÔ´úÂë\n");
+    printf("测试代码\n");
     Martrix_Graph G;
     Create_non_direction_martrix_Graph(&G);
     DFS_Travel(G);
