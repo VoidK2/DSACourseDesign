ac7e8506419797ec6af29e5e927de105082cfe54
VoidK2 <1399477504@qq.com>
2018-03-06 21:14:01 +0800

    Update dfs-无向图-矩阵.cpp

--- dfs-无向图-矩阵.cpp
+++ dfs-无向图-矩阵.cpp
@@ -3,94 +3,79 @@
 #include <cstdbool>
 using namespace std;
 
-#define MAX_VERTEX 100
-#define inf 65535  //表示两点之间没有边相连
+#define MAXN 100
+#define inf 0x3f3f3f3f  //表示两点之间没有边相连
 
-int visit[MAX_VERTEX];   //标记顶点是否被访问
+int visited[MAXN];   //标记顶点是否被访问
 
 /**图的邻接矩阵的建立**/
 //邻接矩阵数据结构定义
-typedef struct Martrix_Graph
+typedef struct Adjacency_Matrix
 {
-    char vertex[MAX_VERTEX]; //存储顶点信息
-    int edge[MAX_VERTEX][MAX_VERTEX]; //存储边信息
-    int vertex_number,edge_number;//存储顶点数和边数
-}Martrix_Graph;
+    char vertex[MAXN]; //存储顶点信息
+    int arc[MAXN][MAXN]; //存储边信息
+    int vertexnum,arcnum;//存储顶点数和边数
+}Adjacency_Matrix;
 
-void Create_non_direction_martrix_Graph( Martrix_Graph *G )
+void Create_non_direction_Adjacency_Matrix( Adjacency_Matrix *M )
 {
-    int i,j,k,m;
-    printf("请输入构造的无向图的顶点数和边数：\n");
-    scanf("%d %d",&G->vertex_number,&G->edge_number);
+    int i,j,k;
+    printf("输入构造的无向图的顶点数和边数：\n");
+    scanf("%d %d",&M->vertexnum,&M->arcnum);
 
-    printf("请输入无向图顶点信息（如ABCDEF....）：\n");
+    printf("输入无向图顶点信息：\n");
     char ch;
     while( ( ch = getchar() != '\n' ) );  //过滤掉前面的\n，防止\n被scanf进去
-    for(i=0;i<G->vertex_number;i++)
-        scanf("%c",&G->vertex[i]);
+    for(i=0;i<M->vertexnum;i++)
+        scanf("%c",&M->vertex[i]);
 
     //不相连的顶点之间的权值设为inf，包括顶点自身
     //初始化邻接矩阵
-    for(i=0;i<G->vertex_number;i++)
-        for(j=0;j<G->vertex_number;j++)
-            G->edge[i][j] = inf;
+    for(i=0;i<M->vertexnum;i++)
+        for(j=0;j<M->vertexnum;j++)
+            M->arc[i][j] = inf;
 
     //更新无向图边信息
-    printf("请输入无向图邻接矩阵相连的边信息，相连标记为1\n");
-    for(k=0;k<G->edge_number;k++)
+    printf("输入无向图邻接矩阵相连的边信息\n");
+    for(k=0;k<M->arcnum;k++)
     {
-        scanf("%d %d %d",&i,&j,&m);
-        G->edge[i][j] = m;
-        G->edge[j][i] = G->edge[i][j];//无向图是对称阵
-    }
-
-
-    //打印邻接矩阵存储信息，检查正确性
-    printf("---------------------构造出来的无向图邻接矩阵如下---------------------\n");
-    for(i=0;i<G->vertex_number;i++)
-    {
-        for(j=0;j<G->vertex_number;j++)
-            printf("%d\t",G->edge[i][j]);
-        printf("\n");
+        scanf("%d %d",&i,&j);
+        M->arc[i][j] = 1;
+        M->arc[j][i] = M->arc[i][j];//无向图是对称阵
     }
 }
 
 //从当前第i个顶点出发，DFS遍历
-void DFS(Martrix_Graph G, int i)
+void DFS(Adjacency_Matrix M, int i)
 {
     int j;
     //标记当前顶点为已访问状态
-    visit[i] = true;
-    printf("%c ",G.vertex[i]);
+    visited[i] = true;
+    printf("%c ",M.vertex[i]);
 
     //对与该顶点相连的其他未访问顶点进行DFS
-    for(j=0;j<G.vertex_number;j++)
+    for(j=0;j<M.vertexnum;j++)
     {
-        if( G.edge[i][j]==1 && !visit[j] )
-            DFS(G,j);
+        if( M.arc[i][j]==1 && !visited[j] )
+            DFS(M,j);
     }
 }
 //对整个图进行DFS
-void DFS_Travel(Martrix_Graph G)
+void DFS_Travel(Adjacency_Matrix M)
 {
-    //初始化visit数组
-    int i;
-    for(i=0;i<G.vertex_number;i++)
-        visit[i] = false;
-
     //整张图的DFS
     printf("对该无向图进行DFS的结果如下：\n");
-    for(i=0;i<G.vertex_number;i++)
+    for(i=0;i<M.vertexnum;i++)
     {
-        if( !visit[i] )
-            DFS(G,i);
+        if( !visited[i] )
+            DFS(M,i);
     }
 }
 int main()
 {
     printf("测试代码\n");
-    Martrix_Graph G;
-    Create_non_direction_martrix_Graph(&G);
-    DFS_Travel(G);
+    Adjacency_Matrix M;
+    Create_non_direction_Adjacency_Matrix(&M);
+    DFS_Travel(M);
     return 0;
 }
