1b7ce9e5fd6dac7e753da8dead1b4660358b9377
1399477504@qq.com <zzx270400>
2018-03-08 21:57:07 +0800

    修改程序

--- DSACourseDesign.cpp
+++ DSACourseDesign.cpp
@@ -1,181 +1,174 @@
-//dfs-递归-邻接矩阵-无向图
-//bfs-非递归-邻接表-有向图
-#include <cstdio>
 #include <iostream>
+#include <cstdio>
 #include <cstring>
-#include <queue>
-#define MAXN 100
-#define inf 0x3f3f3f3f  //表示两点之间没有边相连
+#include <cmath>
+#include <algorithm>
+
+
 using namespace std;
+const int MAXN = 1e4 + 5;
 
-int visited[MAXN + 1];
 
-
-//邻接表定义类型
-typedef struct node {
-	int data;
-	struct node *next;
-} Node, *ArcNode;          //定义边类型
-typedef struct vnode {
-	char data;
-	ArcNode firstarc;
-} VNode;                   //定义结点类型
-typedef struct Adjacency_List {
-	VNode AdjList[MAXN];
-	int vertexnum, arcnum;
-}Adjacency_List;            //定义图类型
-
-//邻接矩阵定义类型
-typedef struct Adjacency_Matrix
+//邻接表节点结构体
+struct Node
 {
-    char vertex[MAXN]; //存储顶点信息
-    int arc[MAXN][MAXN]; //存储边信息
-    int vertexnum,arcnum;//存储顶点数和边数
-} Adjacency_Matrix;
+    int v;                  //表示当前节点
+    struct Node *next;      //表示连接的节点的指针域
+} node[MAXN];
+int que[MAXN];
+int nodenum1,arcnum1;
 
 
-void Init_A_M(Adjacency_Matrix *M)
+//邻接矩阵声明变量
+bool Map[MAXN][MAXN], visited[MAXN];
+int nodenum2,arcnum2;
+
+
+//初始化邻接表
+void BFSInitMap()
 {
-    int i,j,k;
-    printf("输入构造的无向图的顶点数和边数：\n");
-    scanf("%d %d",&M->vertexnum,&M->arcnum);
+    int i,j;
+    for(int p = 1; p <= nodenum1; p++)
+        node[p].v = p, node[p].next = NULL;        //node下标表示当前起始点,所以要初始化为node[i]=i,next=null
 
-    printf("输入无向图顶点信息：\n");
-    char ch;
-    while( ( ch = getchar() != '\n' ) );  //过滤掉前面的\n，防止\n被scanf进去
-    for(i=0; i<M->vertexnum; i++)
-        scanf("%c",&M->vertex[i]);
-
-    //不相连的顶点之间的权值设为inf，包括顶点自身
-    //初始化邻接矩阵
-    for(i=0; i<M->vertexnum; i++)
-        for(j=0; j<M->vertexnum; j++)
-            M->arc[i][j] = inf;
-
-    //更新无向图边信息
-    printf("输入无向图邻接矩阵相连的边信息\n");
-    for(k=0; k<M->arcnum; k++)
+    for(int z=1; z<=arcnum1; z++)
     {
-        scanf("%d %d",&i,&j);
-        M->arc[i][j] = 1;
-        M->arc[j][i] = M->arc[i][j];//无向图是对称阵
+        scanf("%d%d",&i,&j);
+        struct Node *temp = node[i].next;   //先存储当前next指针
+        node[i].next = new Node();          //申请新的内存存储新的通路
+        node[i].next->v = j;                //赋值
+        node[i].next->next = temp;          //将以前的next域串在其后，连起来
+        temp = node[j].next;                //因为无向图，所以不同起点操作一次，再以j为起点操作一次
+        node[j].next = new Node();
+        node[j].next->v = i;
+        node[j].next->next = temp;
     }
 }
-
-int LocateVex(char ch, Adjacency_List L)
-{	//取该结点在数组中的位置
-	int i;
-	for (i = 0; i < L.vertexnum; i++)
-		if (L.AdjList[i].data == ch)
-			return i;
-	return -1;
-}
-void Init_A_L(Adjacency_List &L) {
-	int n, m;
-	int i, j, k;
-	char ch1, ch2;
-	ArcNode p;
-	printf("请输入结点个数和边个数：");
-	scanf("%d%d", &L.vertexnum, &L.arcnum);
-	printf("请输入结点：");
-	getchar();
-	for (i = 0; i < L.vertexnum; i++)
-	{
-		scanf("%c", &L.AdjList[i].data);
-		L.AdjList[i].firstarc = NULL;
-	}
-
-	printf("请输入边：");
-	for (k = 0; k < L.arcnum; k++)
-	{
-		getchar();
-		scanf("%c %c", &ch1, &ch2);
-		i = LocateVex(ch1, L); j = LocateVex(ch2, L);
-
-		ArcNode e;
-		e = (ArcNode)malloc(sizeof(Node));
-		e->data = j;
-		e->next = NULL;
-		p = L.AdjList[i].firstarc;
-		if (!p)
-			L.AdjList[i].firstarc = e;
-		else
-		{
-			while (p->next)
-				p = p->next;
-			p->next = e;
-		}
-	}
-}
-void DFS(Adjacency_Matrix M, int v)
+//广度优先搜索
+void bfs(int now)
 {
-    int j;
-    if (!visited[v])
+    visited[now] = true;                               //将起始点标记为使用过
+    que[++ que[0]] = now;                           //将队列初始化，把起始点加入进去
+    while(que[0] != 0)
     {
-        printf("%c ",M.vertex[v]);
-        visited[v] = 1; //标记为访问过
-    }
-
-    for (j = 0; j < M.vertexnum; j++)
-        if (M.arc[v][j] && !visited[j])   //邻接矩阵的第(v,j)元素不为0
+        //如果队列中还有元素则继续
+        now = que[que[0] --];                       //从队列取出元素，队列元素个数减一
+        struct Node *temp = node[now].next;         //存储其next域
+        while(temp != NULL)
         {
-            //且未被访问过则递归
-            DFS(M, j);
+            //如果next域不为空，则表示还有和该点连通的点
+            if(!visited[temp->v])
+            {
+                //如果该点未被使用则加入队列
+                printf(" -> %d ", temp->v);
+                visited[temp->v] = true;               //将加入的点标记为使用过
+                que[++ que[0]] = temp->v;
+            }
+            temp = temp->next;                      //temp移向下一个next域
         }
-
+    }
+    return ;
 }
-void BFS(Adjacency_List &L) {
-	{	//广度优先搜索
-		queue<int>q;
-		int i, j;
-		ArcNode p;
-        for (i = 0; i < L.vertexnum; i++)
-		{
-			if (!visited[i])
-			{
-				visited[i] = 1;
-				q.push(i);
-				printf("%c ", L.AdjList[i].data);
-				while (!q.empty())
-				{
-					p = L.AdjList[q.front()].firstarc;
-					while (p)
-					{
-						if (!visited[p->data])
-						{
-							visited[p->data] = 1;
-							printf("%c ", L.AdjList[p->data].data);
-							q.push(p->data);
-						}
-						p = p->next;
-					}
-					q.pop();
-				}
-			}
-		}
-	}
 
+void BFS1()
+{
+    memset(que, 0, sizeof(que));            //初始化que队列
+    memset(visited, false, sizeof(visited));//初始化标记使用与否的数组visited
+    printf("请输入顶点和边个数:\n");
+    scanf("%d%d", &nodenum1,&arcnum1);                        //得到输入的顶点数
+    printf("输入边数据:\n");
+    BFSInitMap();                           //进入初始化函数
+    for(int i = 1; i <= nodenum1; i ++)            //同样因为可能不连通需要多次进入
+        if(!visited[i])
+        {
+            //如果未使用则进入bfs
+            printf("从顶点%d开始遍历:\n", i);
+            printf("%d", i);
+            bfs(i);
+            printf("\n");
+        }
+    system("pause");
+    return ;
 }
+
+
+
+
+//初始化Map邻接矩阵
+void DFSInitMap()
+{
+    int i,j;
+    for(int p=1; p<=arcnum2; p++)
+    {
+        scanf("%d%d",&i,&j);
+        Map[i][j] = true;
+    }
+}
+//深度优先搜索
+void dfs(int now)
+{
+    visited[now] = true;                       //进入后将当前节点标记为已使用
+    for(int i = 1; i <= nodenum2; i ++)
+        if(Map[now][i] && !visited[i])         //以当前节点为起始点寻找下一个未使用的节点
+        {
+            printf(" -> %d ", i);
+            dfs(i);                         //找到后输出并以此节点进入dfs递归
+        }
+    return ;
+}
+
+void DFS1()
+{
+    char str1[10];
+    memset(visited, false, sizeof(visited));
+    printf("输入顶点和边的个数:\n");
+    scanf("%d%d", &nodenum2,&arcnum2);
+    printf("输入边的数据:\n");
+    DFSInitMap();
+    for(int i = 1; i <= nodenum2; i ++)
+    {
+        //如果图不连通，则需要多次调用
+        if(!visited[i])                         //如果该节点未被使用则进入
+        {
+            printf("从顶点%d开始遍历:\n", i);
+            printf("%d", i);
+            dfs(i);
+            printf("\n");
+        }
+    }
+    system("pause");
+    return ;
+}
+
+
+
+
 int main()
 {
-//    freopen("input.txt","r",stdin);
-//    freopen("out.txt","w",stdout);
-
-	Adjacency_List L;
-	printf("广度优先搜索-非递归-邻接表-有向图的遍历：\n");
-	Init_A_L(L);
-	ArcNode p;
-	p = L.AdjList[0].firstarc;
-	memset(visited, 0, sizeof(visited));
-	printf("遍历结果：\n");
-	BFS(L);
-	putchar('\n');
-
-    Adjacency_Matrix M;
-    memset(visited, 0, sizeof(visited));
-    printf("深度优先搜索-递归-邻接矩阵-无向图的遍历：\n");
-    Init_A_M(&M);
-    printf("遍历结果：\n");
-    DFS(M, 0);
-    putchar('\n');
+    int operation;
+    while(true)
+    {
+        printf("*************************************\n");
+        printf("   1.有向图邻接矩阵dfs递归遍历\n");
+        printf("   2.无向图邻接表bfs非递归遍历\n");
+        printf("   0.退出\n");
+        printf("*************************************\n");
+        scanf("%d", &operation);
+        switch(operation)
+        {
+        case 1:
+            DFS1();      //为1进入DFSMAIN
+            break;
+        case 2:
+            BFS1();      //为2进入BFSMAIN
+            break;
+        case 0:
+            return 0;       //为0退出程序
+        default:
+            printf("input error\n");
+        }
+        system("CLS");      //清除屏幕
+    }
+    return 0;
 }
+
